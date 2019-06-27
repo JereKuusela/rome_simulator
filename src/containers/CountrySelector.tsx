ddf5c95a0f679dc2677beda82deb0c7c20b6a210
Jere Kuusela <jere.kuusela@iki.fi>
2019-06-28 00:09:46 +0300

    Ui work for Countries, traditions can be selected

--- src/containers/CountrySelector.tsx
+++ src/containers/CountrySelector.tsx
@@ -1,47 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Modal } from 'semantic-ui-react'
-import { UnitType, ArmyName, ArmyType, ValueType, Unit, UnitDefinition } from '../store/units'
-import { selectUnit } from '../store/battle'
+import DropdownSelector from '../components/DropdownSelector'
 import { AppState } from '../store/'
-import { getBattle, filterTerrainTypes } from '../utils'
-import { addValues, mergeValues, ValuesType, DefinitionType } from '../base_definition'
-import ItemRemover from '../components/ItemRemover'
-import UnitDetail from '../components/UnitDetail'
+import { selectCountry } from '../store/settings'
+import { CountryName } from '../store/countries'
 
-const CUSTOM_VALUE_KEY = 'Unit'
-
-export interface ModalInfo {
-  name: ArmyName
-  index: number
-  type: ArmyType
-}
 
 class CountrySelector extends Component<IProps> {
 
-  unit: Unit = undefined!
-
   render(): JSX.Element | null {
-    if (!this.props.info)
-      return null
     return (
-      <Modal basic onClose={this.props.onClose} open>
-      </Modal>
+      <DropdownSelector
+        items={this.props.countries.keySeq()}
+        active={this.props.active}
+        onSelect={this.props.selectCountry}
+      />
     )
   }
 }
 
 const mapStateToProps = (state: AppState) => ({
-
+  active: state.settings.country,
+  countries: state.countries.selections
 })
 
 const mapDispatchToProps = (dispatch: any) => ({
+  selectCountry: (country: CountryName) => (dispatch(selectCountry(country)))
 
 })
 
 interface IProps extends ReturnType<typeof mapStateToProps>, ReturnType<typeof mapDispatchToProps> {
-  info: ModalInfo | null
-  onClose: () => void
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CountrySelector)
--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -3,14 +3,21 @@ import { Container, Grid, Table, List, Header } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { AppState } from '../store/index'
 import { mapRange } from '../utils'
-import { ModifierType, Modifier } from '../store/countries'
+import { ModifierType, Modifier, Tradition, enableTradition, clearTradition, CountryName } from '../store/countries'
+import CountrySelector from '../containers/CountrySelector'
 
 class Countries extends Component<IProps> {
 
   render(): JSX.Element {
+    const selections = this.props.selections.get(this.props.country)!.traditions
     return (
       <Container>
-        <Grid padded celled>
+        <Grid>
+          <Grid.Row columns='1'>
+            <Grid.Column>
+              <CountrySelector />
+            </Grid.Column>
+          </Grid.Row>
           {
             this.props.tradition_types.map(type => {
               const tradition = this.props.traditions.get(type)
@@ -21,7 +28,7 @@ class Countries extends Component<IProps> {
                 <Grid.Row columns='1'>
                   <Grid.Column key={type}>
                     <Header>{tradition.type}</Header>
-                    <Table celled selectable unstackable>
+                    <Table celled unstackable>
                       <Table.Header>
                         <Table.Row>
                           {
@@ -33,38 +40,53 @@ class Countries extends Component<IProps> {
                           }
                         </Table.Row>
                       </Table.Header>
-                      {
-                        mapRange(rows, number => number).map(number => (
-                          <Table.Row key={number}>
-                            {
-                              tradition.paths.map(path => {
-                                const tradition = path.traditions.get(number)
-                                if (!tradition)
-                                  return null
-                                return (
-                                  <Table.Cell key={number + '_' + path.name}>
-                                    <List>
-                                      {
-                                        tradition.modifiers.map(modifier => (
-                                          <List.Item>
-                                            {
-                                              this.getText(modifier)
-                                            }
-                                            {
-                                              this.getValue(modifier)
-                                            }
-                                          </List.Item>
-                                        ))
+                      <Table.Body>
+                        {
+                          mapRange(rows, number => number).map(row => (
+                            <Table.Row key={row}>
+                              {
+                                tradition.paths.map((path, column) => {
+                                  const tradition = path.traditions.get(row)
+                                  if (!tradition)
+                                    return null
+                                  const key = 'tradition_' + column + '_' + row
+                                  const previous_taken = row === 0 || selections.has('tradition_' + column + '_' + (row - 1))
+                                  const next_taken = selections.has('tradition_' + column + '_' + (row + 1))
+                                  return (
+                                    <Table.Cell
+                                      key={row + '_' + path.name}
+                                      positive={selections.has(key)}
+                                      selectable
+                                      disabled={next_taken || !previous_taken}
+                                      onClick={
+                                        selections.has(key)
+                                          ? () => this.props.clearTradition(this.props.country, key)
+                                          : () => this.props.enableTradition(this.props.country, key, tradition)
                                       }
-                                    </List>
-                                  </Table.Cell>
-                                )
-                              })
-                            }
-                          </Table.Row>
-                        ))
-                      }
-
+                                      style={{padding: '.78571429em .78571429em'}}
+                                    >
+                                      <List>
+                                        {
+                                          tradition.modifiers.map(modifier => (
+                                            <List.Item>
+                                              {
+                                                this.getText(modifier)
+                                              }
+                                              {
+                                                this.getValue(modifier)
+                                              }
+                                            </List.Item>
+                                          ))
+                                        }
+                                      </List>
+                                    </Table.Cell>
+                                  )
+                                })
+                              }
+                            </Table.Row>
+                          ))
+                        }
+                      </Table.Body>
                     </Table>
                   </Grid.Column>
                 </Grid.Row>
@@ -86,7 +108,7 @@ class Countries extends Component<IProps> {
     if (!modifier.value)
       return null
     const sign = modifier.value > 0 ? '+' : ''
-    return <span className='positive' style={{ float: 'right' }}>{sign + modifier.value * 100} %</span>
+    return <span className='value-positive' style={{ float: 'right' }}>{sign + modifier.value * 100} %</span>
   }
 }
 
@@ -94,10 +116,13 @@ const mapStateToProps = (state: AppState) => ({
   traditions: state.countries.traditions.definitions,
   tradition_types: state.countries.traditions.types,
   selections: state.countries.selections,
+  country: state.settings.country,
   mode: state.settings.mode
 })
 
 const mapDispatchToProps = (dispatch: any) => ({
+  enableTradition: (country: CountryName, key: string, tradition: Tradition) => (dispatch(enableTradition(country, key, tradition))),
+  clearTradition: (country: CountryName, key: string) => (dispatch(clearTradition(country, key)))
 })
 
 interface IProps extends ReturnType<typeof mapStateToProps>, ReturnType<typeof mapDispatchToProps> { }
