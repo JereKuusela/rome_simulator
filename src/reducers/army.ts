def3864f0dea294d9bf2bd0eabd39969f334e895
Jere Kuusela <jere.kuusela@iki.fi>
2020-02-08 21:58:48 +0200

    Refactored army reducer

--- src/reducers/army.ts
+++ src/reducers/army.ts
@@ -1,53 +1,38 @@
 
-import { produce } from 'immer'
-import { Prepend } from 'typescript-tuple'
-import { Army, CountryName } from 'types'
+import { Army, CountryName, Countries } from 'types'
 import { ArmyName } from 'types/armies'
 import * as manager from 'managers/army_manager'
 import { getDefaultCountryDefinitions } from 'data'
 import { ReducerParams } from 'state'
+import { ActionToFunction, makeActionReplaceFirst, makeReducer, Action } from './utils'
 
-const actionToFunction: { [key: string]: (army: Army, ...args: any) => void | undefined } = {}
+const actionToFunction: ActionToFunction<Army> = {}
 
-const makeAction = <T extends any[], S extends string>(func: (army: Army, ...args: T) => any, type: S) => {
-  const ret = (country: CountryName, ...args: T) => ({
-    type,
-    payload: [country, ...args] as any as  Prepend<T, CountryName>
-  })
-  actionToFunction[type] = func
-  ret['type'] = type
-  return ret
+export const selectCohort = makeActionReplaceFirst(manager.selectCohort, 'selectCohort' as CountryName, actionToFunction)
+export const toggleCohortLoyality = makeActionReplaceFirst(manager.toggleCohortLoyality, 'toggleCohortLoyality' as CountryName, actionToFunction)
+export const setCohortValue = makeActionReplaceFirst(manager.setCohortValue, 'setCohortValue' as CountryName, actionToFunction)
+export const changeCohortType = makeActionReplaceFirst(manager.changeCohortType, 'changeCohortType' as CountryName, actionToFunction)
+export const editCohort = makeActionReplaceFirst(manager.editCohort, 'editCohort' as CountryName, actionToFunction)
+export const deleteCohort = makeActionReplaceFirst(manager.deleteCohort, 'deleteCohort' as CountryName, actionToFunction)
+export const removeFromReserve = makeActionReplaceFirst(manager.removeFromReserve, 'removeFromReserve' as CountryName, actionToFunction)
+export const addToReserve = makeActionReplaceFirst(manager.addToReserve, 'addToReserve' as CountryName, actionToFunction)
+export const clearCohorts = makeActionReplaceFirst(manager.clearCohorts, 'clearCohorts' as CountryName, actionToFunction)
+export const selectTactic = makeActionReplaceFirst(manager.selectTactic, 'selectTactic' as CountryName, actionToFunction)
+export const setFlankSize = makeActionReplaceFirst(manager.setFlankSize, 'setFlankSize' as CountryName, actionToFunction)
+export const setUnitPreference = makeActionReplaceFirst(manager.setUnitPreference, 'setUnitPreference' as CountryName, actionToFunction)
+export const setGeneralMartial = makeActionReplaceFirst(manager.setGeneralMartial, 'setGeneralMartial' as CountryName, actionToFunction)
+export const setHasGeneral = makeActionReplaceFirst(manager.setHasGeneral, 'setHasGeneral' as CountryName, actionToFunction)
+export const clearModifiers = makeActionReplaceFirst(manager.clearModifiers, 'clearModifiers' as CountryName, actionToFunction)
+export const enableModifiers = makeActionReplaceFirst(manager.enableModifiers, 'enableModifiers' as CountryName, actionToFunction)
+
+const getEntity = (draft: Countries, action: Action<CountryName>, params: ReducerParams) => {
+  const [country] = action.payload
+  return draft[country].armies[params.mode][ArmyName.Army1]
 }
 
-export const selectCohort = makeAction(manager.selectCohort, 'selectCohort')
-export const toggleCohortLoyality = makeAction(manager.toggleCohortLoyality, 'toggleCohortLoyality')
-export const setCohortValue = makeAction(manager.setCohortValue, 'setCohortValue')
-export const changeCohortType = makeAction(manager.changeCohortType, 'changeCohortType')
-export const editCohort = makeAction(manager.editCohort, 'editCohort')
-export const deleteCohort = makeAction(manager.deleteCohort, 'deleteCohort')
-export const removeFromReserve = makeAction(manager.removeFromReserve, 'removeFromReserve')
-export const addToReserve = makeAction(manager.addToReserve, 'addToReserve')
-export const clearCohorts = makeAction(manager.clearCohorts, 'clearCohorts')
-export const selectTactic = makeAction(manager.selectTactic, 'selectTactic')
-export const setFlankSize = makeAction(manager.setFlankSize, 'setFlankSize')
-export const setUnitPreference = makeAction(manager.setUnitPreference, 'setUnitPreference')
-export const setGeneralMartial = makeAction(manager.setGeneralMartial, 'setGeneralMartial')
-export const setHasGeneral = makeAction(manager.setHasGeneral, 'setHasGeneral')
-export const clearModifiers = makeAction(manager.clearModifiers, 'clearModifiers')
-export const enableModifiers = makeAction(manager.enableModifiers, 'enableModifiers')
-
-export const armyReducer = (state = getDefaultCountryDefinitions(), action: Action, params: ReducerParams) => {
-  const func = actionToFunction[action.type]
-  if (!func)
-    return state
-  return produce(state, draft => {
-    const [country, ...payload] = action.payload
-    const army = draft[country].armies[params.mode]
-    func(army[ArmyName.Army1], ...payload)
-  })
+const getEntityPayload = (action: Action<CountryName>) => {
+  const [, ...payload] = action.payload
+  return payload
 }
 
-type Action = {
-  type: string,
-  payload: [CountryName, ...any[]]
-}
+export const armyReducer = makeReducer(getDefaultCountryDefinitions(), actionToFunction, getEntity, getEntityPayload)
