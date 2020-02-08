ba5d5707f758f4ef4c90edf2915f6687374ebc1a
Jere Kuusela <jere.kuusela@iki.fi>
2020-02-08 21:53:57 +0200

    Refactored settings reducer

--- src/reducers/settings.ts
+++ src/reducers/settings.ts
@@ -1,72 +1,16 @@
-import { ImmerReducer, createActionCreators, createReducerFunction, Actions } from 'immer-reducer'
 
-import { has } from 'utils'
-import { DefinitionType, Mode, CountryName, Side, SimulationSpeed, CombatSettings, SiteSettings, Setting, SettingsAndOptions, WearinessAttribute } from 'types'
-import { getDefaultSettings, speedValues } from 'data'
+import * as manager from 'managers/settings'
+import { makeActionRemoveFirst, makeContainerReducer, ActionToFunction } from './utils'
+import { SettingsAndOptions } from 'types'
+import { getDefaultSettings } from 'data'
 
-class SettingsReducer extends ImmerReducer<SettingsAndOptions> {
+const actionToFunction: ActionToFunction<SettingsAndOptions> = {}
 
-  changeCombatParameter(mode: Mode, key: keyof CombatSettings, value: number | boolean | string) {
-    this.draftState.combatSettings[mode][key] = value as never
-  }
+export const changeCombatParameter = makeActionRemoveFirst(manager.changeCombatParameter, 'changeCombatParameter', actionToFunction)
+export const changeSiteParameter = makeActionRemoveFirst(manager.changeSiteParameter, 'changeSiteParameter', actionToFunction)
+export const changeWeariness = makeActionRemoveFirst(manager.changeWeariness, 'changeWeariness', actionToFunction)
+export const selectCountry = makeActionRemoveFirst(manager.selectCountry, 'selectCountry', actionToFunction)
+export const toggleAccordion = makeActionRemoveFirst(manager.toggleAccordion, 'toggleAccordion', actionToFunction)
+export const toggleMode = makeActionRemoveFirst(manager.toggleMode, 'toggleMode', actionToFunction)
 
-  changeSiteParameter(key: keyof SiteSettings, value: number | boolean | string) {
-    if (key === Setting.Performance && typeof value === 'string' && speedValues[value]) {
-      this.draftState.siteSettings[Setting.PhaseLengthMultiplier] = speedValues[value][0]
-      this.draftState.siteSettings[Setting.MaxDepth] = speedValues[value][1]
-    }
-    if (key === Setting.PhaseLengthMultiplier || key === Setting.MaxDepth) {
-      this.draftState.siteSettings[Setting.Performance] = SimulationSpeed.Custom
-    }
-    this.draftState.siteSettings[key] = value as never
-  }
-
-  toggleMode() {
-    this.draftState.mode = this.state.mode === DefinitionType.Land ? DefinitionType.Naval : DefinitionType.Land
-  }
-
-  selectCountry(country: CountryName) {
-    this.draftState.country = country
-  }
-
-  changeCountryName(old_country: CountryName, country: CountryName) {
-    if (this.state.country === old_country)
-      this.selectCountry(country)
-  }
-
-  createCountry(country: CountryName) {
-    this.selectCountry(country)
-  }
-
-  deleteCountry(country: CountryName) {
-    if (this.state.country === country)
-      this.selectCountry('' as CountryName)
-  }
-
-  toggleAccordion(key: string) {
-    if (has(this.state.accordions, key))
-      delete this.draftState.accordions[key]
-    else
-      this.draftState.accordions[key] = true
-  }
-
-  changeWeariness(side: Side, type: WearinessAttribute, min: number, max: number) {
-    this.draftState.weariness[side][type].min = min
-    this.draftState.weariness[side][type].max = max
-  }
-}
-
-const actions = createActionCreators(SettingsReducer)
-
-export const changeCombatParameter = actions.changeCombatParameter
-export const changeSiteParameter = actions.changeSiteParameter
-export const selectCountry = actions.selectCountry
-export const toggleAccordion = actions.toggleAccordion
-export const toggleMode = actions.toggleMode
-export const changeWeariness = actions.changeWeariness
-
-export const reducer = createReducerFunction(SettingsReducer, getDefaultSettings())
-
-export const settingsReducer = (state = getDefaultSettings(), action: Actions<typeof SettingsReducer>) => {
-  return reducer(state, action)
-}
+export const settingsReducer = makeContainerReducer(getDefaultSettings(), actionToFunction)
