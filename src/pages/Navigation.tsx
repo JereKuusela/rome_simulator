51ec0f5fb0d907cff8e80897bab05678820df641
Jere Kuusela <jere.kuusela@iki.fi>
2020-11-03 02:49:52 +0200

    Navigation refactored to use hooks

--- src/pages/Navigation.tsx
+++ src/pages/Navigation.tsx
@@ -1,80 +1,75 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { AppState, getMode } from 'state'
 import { setMode } from 'reducers'
 import { Menu, Image, Button } from 'semantic-ui-react'
 import IconLand from 'images/land_combat.png'
 import IconNaval from 'images/naval_combat.png'
 import { Mode } from 'types'
+import { useHistory } from 'react-router-dom'
 
 interface Props { }
 
 /**
  * Navigation menu for different pages.
  */
-class Navigation extends Component<IProps> {
+const Navigation = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const path = history.location.pathname
 
-  render() {
-    const { mode, setMode } = this.props
-    const path = (this.props as any).location.pathname
-    const history = (this.props as any).history
+  const currentMode = useSelector(getMode)
+
+  const renderModeButton = (mode: Mode) => {
     return (
-      <div id='navigation'>
-        <Menu>
-          <Menu.Item active={path === '/'} onClick={() => history.push('/')}>
-            Battle
-          </Menu.Item>
-          <Menu.Item active={path === '/analyze'} onClick={() => history.push('/analyze')}>
-            Analyze
-          </Menu.Item>
-          <Menu.Item active={path === '/countries'} onClick={() => history.push('/countries')}>
-            Countries
-          </Menu.Item>
-          <Menu.Item active={path === '/definitions'} onClick={() => history.push('/definitions')}>
-            Definitions
-          </Menu.Item>
-          <Menu.Item active={path === '/settings'} onClick={() => history.push('/settings')}>
-            Settings
-          </Menu.Item>
-          {process.env.REACT_APP_GAME === 'IR' &&
-            <Menu.Item active={path === '/import'} onClick={() => history.push('/import')}>
-              Import from save
-          </Menu.Item>
-          }
-          {process.env.REACT_APP_GAME === 'IR' &&
-            <Menu.Item active={path === '/export'} onClick={() => history.push('/export')}>
-              Export from save
-          </Menu.Item>
-          }
-
-          <div id='menu-info'>
-            {process.env.REACT_APP_GAME === 'IR' &&
-              <Button active={mode === Mode.Land} compact icon basic circular size='tiny' onClick={() => setMode(Mode.Land)}>
-                <Image src={IconLand} avatar style={{ marginRight: 0 }} />
-              </Button>
-            }
-            {process.env.REACT_APP_GAME === 'IR' &&
-              <Button active={mode === Mode.Naval} compact icon basic circular size='tiny' onClick={() => setMode(Mode.Naval)}>
-                <Image src={IconNaval} avatar style={{ marginRight: 0 }} />
-              </Button>
-            }
-            <div id='version'><div>Site version 0.8.2</div><div>Game version {process.env.REACT_APP_GAME === 'ir' ? '1.5.3' : '1.30.4'}</div></div>
-          </div>
-        </Menu>
-        <br />
-      </div >
+      <Button active={mode === currentMode} compact icon basic circular size='tiny' onClick={() => dispatch(setMode(mode))}>
+        <Image src={mode == Mode.Land? IconLand: IconNaval} avatar style={{ marginRight: 0 }} />
+      </Button>
     )
   }
+
+  return (
+    <div id='navigation'>
+      <Menu>
+        <Menu.Item active={path === '/'} onClick={() => history.push('/')}>
+          Battle
+          </Menu.Item>
+        <Menu.Item active={path === '/analyze'} onClick={() => history.push('/analyze')}>
+          Analyze
+          </Menu.Item>
+        <Menu.Item active={path === '/countries'} onClick={() => history.push('/countries')}>
+          Countries
+          </Menu.Item>
+        <Menu.Item active={path === '/definitions'} onClick={() => history.push('/definitions')}>
+          Definitions
+          </Menu.Item>
+        <Menu.Item active={path === '/settings'} onClick={() => history.push('/settings')}>
+          Settings
+          </Menu.Item>
+        {process.env.REACT_APP_GAME === 'IR' &&
+          <Menu.Item active={path === '/import'} onClick={() => history.push('/import')}>
+            Import from save
+          </Menu.Item>
+        }
+        {process.env.REACT_APP_GAME === 'IR' &&
+          <Menu.Item active={path === '/export'} onClick={() => history.push('/export')}>
+            Export from save
+          </Menu.Item>
+        }
+
+        <div id='menu-info'>
+          {process.env.REACT_APP_GAME === 'IR' &&
+            renderModeButton(Mode.Land)
+          }
+          {process.env.REACT_APP_GAME === 'IR' &&
+            renderModeButton(Mode.Naval)
+          }
+          <div id='version'><div>Site version 0.8.2</div><div>Game version {process.env.REACT_APP_GAME === 'IR' ? '1.5.3' : '1.30.4'}</div></div>
+        </div>
+      </Menu>
+      <br />
+    </div >
+  )
 }
 
-const mapStateToProps = (state: AppState) => ({
-  mode: getMode(state)
-})
-
-const actions = { setMode }
-
-type S = ReturnType<typeof mapStateToProps>
-type D = typeof actions
-interface IProps extends React.PropsWithChildren<Props>, S, D { }
-
-export default connect(mapStateToProps, actions)(Navigation)
+export default Navigation
